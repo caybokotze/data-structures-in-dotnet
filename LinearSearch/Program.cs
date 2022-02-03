5877f0fe2e968a9aa63ab24d270f8a52420f7a4f
Kotze <caybo.kotze@capgemini.com>
2022-02-03 10:23:57 +0100

    :sparkles: add linear search

---
+++ LinearSearch/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Shared;
+
+var a = TestData.Naturals();
+var n = a.Length;
+var s = 0;
+
+var item = 7;
+
+for (var i = 1; i <= n - 1; i++)
+{
+    if (a[i] == item)
+    {
+        s = 1;
+        break;
+    }
+}
+
+if (s == 0)
+{
+    Console.WriteLine("No data found");
+}
